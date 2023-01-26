9c25dda193777345940a007d65a3d36e3f45fd4a
ujwalthakare <ujwalthakare2@gmail.com>
2023-01-26 21:34:54 +0530

    first commit

---
+++ DemoApp/src/ineuron/java/DemoApp.java
@@ -0,0 +1,15 @@
+package ineuron.java;
+
+public class DemoApp {
+
+	public static void main(String[] args) {
+		int [] a = {1,2,3};
+		for (int i : a) {
+			System.out.println(i);
+		}
+      String name = "luffy";
+      int count = name.toUpperCase().length();
+      System.out.println(count);
+	}
+
+}
